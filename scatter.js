5e5ced90a850904934d597126d2e5b0ec09970f3
millerliam <miller.lia@northeastern.edu>
2024-10-27 23:00:57 -0400

    Update scatter.js

--- scatter.js
+++ scatter.js
@@ -12,6 +12,8 @@ const svg = d3.select("#scatterPlot")
 
 // Load the Iris data
 d3.csv("iris.csv").then(function(data) {
+    console.log(data);  // Log the data to ensure it is being loaded
+
     // Convert data to numeric
     data.forEach(d => {
         d.petalLength = +d.petalLength;
@@ -36,7 +38,7 @@ d3.csv("iris.csv").then(function(data) {
     svg.append("g")
         .attr("transform", `translate(0,${height})`)
         .call(d3.axisBottom(xScale));
-    
+
     svg.append("g")
         .call(d3.axisLeft(yScale));
 
